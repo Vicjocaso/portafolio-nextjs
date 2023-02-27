61c3218dfb3fe8fbec42014ce2ec9fd8656a6166
vicjocaso <vicjocaso@gmail.com>
2023-02-27 14:11:10 -0500

    first commit

---
+++ components/About.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="w-full md:h-screen p-2 flex items-center">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-red-600">
+            About
+          </p>
+          <h2 className="py-4">Who I am</h2>
+          <p className="py-4 text-gray-600">Developer</p>
+          <p className="py-4 text-gray-600">
+            A full-stack developer is a developer or engineer who can build both
+            the front end and the back end of a website. The front end (the
+            parts of a website a user sees and interacts with) and the back end
+            (the behind-the-scenes data storage and processing) require
+            different skill sets. Since full-stack developers are involved with
+            all aspects of the development process, they must have expertise in
+            both.
+          </p>
+          <p className="py-4 text-gray-600">
+            A full-stack developer is a developer or engineer who can build both
+            the front end and the back end of a website. The front end (the
+            parts of a website a user sees and interacts with) and the back end
+            (the behind-the-scenes data storage and processing) require
+            different skill sets. Since full-stack developers are involved with
+            all aspects of the development process, they must have expertise in
+            both.
+          </p>
+          <p className="py-4 text-gray-600">link</p>
+        </div>
+        <div className="w-full h-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 cursor-pointer hover:scale-105 ease-in duration-300">
+          <Image
+            className="rounded-xl w-full h-full"
+            src="/../public/assets/code.png"
+            alt="code Image"
+            width="1244"
+            height="1200"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ components/HomePage.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { BsFillPersonFill } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto flex justify-center items-center">
+        <div>
+          <p>Hello from HomePage Page</p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I'm <span className="text-red-600">Victor</span>
+          </h1>
+          <h1 className="py-4 text-gray-700">
+            A Full-Stack Web
+            <span className="text-red-600"> Developer</span>
+          </h1>
+          <p className="py-2 text-gray-600 max-w-[70%] m-auto">
+            I'm a full-stack web developer bla bla bl al bla
+            blaaskdjaskldjaskoldjaskldjasl rem
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+              <Link href="https://www.linkedin.com/in/victor-jos%C3%A9-calder%C3%B3n-16a194184/">
+                <FaLinkedin size={25} />
+              </Link>
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+              <Link href="https://github.com/Vicjocaso">
+                <FaGithub size={25} />
+              </Link>
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail size={25} />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsFillPersonFill size={25} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
