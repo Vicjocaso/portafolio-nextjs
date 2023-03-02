1916307596ae1fc038b4cf2ff3baec66968ef350
vicjocaso <vicjocaso@gmail.com>
2023-03-01 22:27:41 -0500

    move skills imanges folder

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -8,48 +8,40 @@ const Skills = () => {
         <p className="uppercase text-xl tracking-widest text-red-600">Skills</p>
         <h2 className="py-4">What I Can Do</h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <SkillBox src="/../public/skills/html.png" alt="html" title="HTML" />
+          <SkillBox src="/../public/skills/css.png" alt="Css" title="Css" />
           <SkillBox
-            src="/../public/assets/skills/html.png"
-            alt="html"
-            title="HTML"
-          />
-          <SkillBox
-            src="/../public/assets/skills/css.png"
-            alt="Css"
-            title="Css"
-          />
-          <SkillBox
-            src="/../public/assets/skills/nextjs.png"
+            src="/../public/skills/nextjs.png"
             alt="NextJs"
             title="NextJS"
           />
           <SkillBox
-            src="/../public/assets/skills/node.png"
+            src="/../public/skills/node.png"
             alt="Node"
             title="NodeJs"
           />
           <SkillBox
-            src="/../public/assets/skills/tailwind.png"
+            src="/../public/skills/tailwind.png"
             alt="Taildwind"
             title="Tailwind"
           />
           <SkillBox
-            src="/../public/assets/skills/github.png"
+            src="/../public/skills/github.png"
             alt="GitHub"
             title="GitHub"
           />
           <SkillBox
-            src="/../public/assets/skills/reactjs.png"
+            src="/../public/skills/reactjs.png"
             alt="ReactJs"
             title="ReactJs"
           />
           <SkillBox
-            src="/../public/assets/skills/typescript.png"
+            src="/../public/skills/typescript.png"
             alt="TypeScript"
             title="TypeScript"
           />
           <SkillBox
-            src="/../public/assets/skills/golang.png"
+            src="/../public/skills/golang.png"
             alt="Golang"
             title="Golang"
           />
